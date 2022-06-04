1bd270bffd2c074e9a4d8de9756bb56250e0eff2
Sanjay Verma <2849sanjay@gmail.com>
2022-06-04 15:28:26 +0530

    task5

---
+++ Relevel_Batch1n2_MBA-main/utils/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+    userType : {
+        admin : "ADMIN",
+        customer : "CUSTOMER",
+        theatreOwner : "THEATRE_OWNER"
+    },
+    releaseStatuses : {
+        released: "RELEASED",
+        coming_soon: "COMING SOON"
+    },
+    languages : {
+        hindi: "HINDI",
+        english:"ENGLISH",
+        tamil:"TAMIL"
+    }
+};
